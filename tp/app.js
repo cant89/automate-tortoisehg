d02bbdd0af4f286067df8d47b9d3dcb88a1ff1ac
Mantovani Gabriele <tech821@yoox.com>
2016-02-25 18:11:15 +0100

    Update Tags field

--- tp/app.js
+++ tp/app.js
@@ -4,21 +4,40 @@
 *
 */
 
+// Include config file
+var config = require('app-config');
+
+var idTP = process.argv.slice(2);
+
 var request = require('request'),
-    username = "",
-    password = "",
-    url = "",
+    username = config.tp.username,
+    password = config.tp.password,
+    url = config.tp.url,
     auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
 
-request(
-    {
-        url : url,
-        headers : {
-            "Authorization" : auth
-        }
-    },
-    function (error, response, body) {
-        // Do more stuff with 'body' here
-		console.log(body);
-    }
-);
+if(idTP) {
+	
+	idTP = parseInt(idTP);
+	
+	var postData = {
+		Id: idTP,
+		Tags: "Ready to Deploy"
+	};
+	
+	request(
+		{
+			headers : {
+				"Authorization" : auth
+			},
+			method: 'post',
+			body: postData,
+			json: true,
+			url : url + "api/v1/Bugs/?resultFormat=json&resultInclude=[Id,Tags]"
+		},
+		function (error, response, body) {
+			console.log(body);
+		}
+	);	
+}
+	
+
